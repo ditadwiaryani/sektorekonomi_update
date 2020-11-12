880f405f7f4e94a6706a69d95cd6e991bdf0e795
Mostrans Opr <admin-opr@mostrans.co.id>
2020-11-12 20:24:19 +0700

    master

---
+++ QgisToWeb/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QgisToWeb.Models;
+
+namespace QgisToWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet("linkform")]
+        public IActionResult LinkForm()
+        {
+            return View();
+        }
+
+        [HttpGet("linkpemkot")]
+        public IActionResult LinkPemkot()
+        {
+            return View();
+        }
+
+        [HttpGet("news")]
+        public IActionResult News()
+        {
+            return View();
+        }
+
+        [HttpGet("webmap")]
+        public IActionResult WebMap()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
