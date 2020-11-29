6029c7089e2ae617f935d623139285857478393e
Mostrans Opr <admin-opr@mostrans.co.id>
2020-11-29 23:22:48 +0700

    fix version

---
+++ qgis2web_downgrade/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace qgis2web_downgrade.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
