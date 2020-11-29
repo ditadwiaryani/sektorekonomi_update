8522f8c21053e67cbf00c593f6dbdb0a4d50722f
Mostrans Opr <admin-opr@mostrans.co.id>
2020-11-29 23:00:25 +0700

    fix version

--- QgisToWeb/obj/Release/netcoreapp3.0/PubTmp/Out/wwwroot/qgis/data/Amphitheater_1.js
+++
@@ -1,8 +0,0 @@
-var json_Amphitheater_1 = {
-"type": "FeatureCollection",
-"name": "Amphitheater_1",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Latitude": -7.8081349, "longitude": 110.3703695, "Nama Usaha": "Mandira Baruga", "Alamat Len": "Purawisata  Kawasan Mandira Baruga  Jl. Brigjen Katamso  Keparakan  Kec. Mergangsan  Kota Yogyakarta  Daerah Istimewa Yogyakarta 55152", "Jalan": "Purawisata", "Jalan2": "Kawasan Mandira Baruga", "Kelurahan": "Keparakan", "RW": null, "RT": null, "email": null, "No Telepon": "0857-0117-2777", "Jenis Usah": "Amphitheater|Convention center|Indonesian restaurant|Wedding service|Wedding venue|Western restaurant", "website": "https:\/\/amazingramayanaballet.com\/", "domain": "amazingramayanaballet.com", "google_sea": "https:\/\/www.google.co.id\/search?q=Mandira Baruga  Purawisata  Kawasan Mandira Baruga  Jl. Brigjen Katamso  Keparakan  Kec. Mergangsan  Kota Yogyakarta  Daerah Istimewa Yogyakarta 55152", "facebook_u": null, "twitter_ur": null, "instagram_": null, "youtube_ur": null, "pinterest_": null }, "geometry": { "type": "Point", "coordinates": [ 110.3703695, -7.8081349 ] } }
-]
-}
